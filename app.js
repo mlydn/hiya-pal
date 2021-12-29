ee678fcd1a2599feb5912f07c505a2cd3ba09ab0
Martin Lyden <martin.lyden@dazn.com>
2021-12-29 11:14:20 +0100

    don't lower your standards

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-//Require module
-const express = require('express');
+// Require module
+const express = require('express')
 // Express Initialize
-const app = express();
+const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-//create api
-app.get('/hiya_pal', (req,res)=>{
-  res.json({ message: 'Hiya Pal!' });
+// create api
+app.get('/hiya_pal', (req, res) => {
+  res.json({ message: 'Hiya Pal!' })
 })
 
-app.get('/pal/:name', (req, res)=>{
-  res.json({ message: 'Hiya ' + capitalizeFirstLetter(req.params.name) + '!'});
+app.get('/pal/:name', (req, res) => {
+  res.json({ message: 'Hiya ' + capitalizeFirstLetter(req.params.name) + '!' })
 })
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js')
-const port = 8000;
-const host = '0.0.0.0';
+const port = 8000
+const host = '0.0.0.0'
 
-app.listen(port,host, ()=> {
-  console.log(`Running on http://${host}:${port}`);
+app.listen(port, host, () => {
+  console.log(`Running on http://${host}:${port}`)
 })
